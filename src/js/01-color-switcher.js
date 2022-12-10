048b9f0266a4cc2025b45cc5c3b85c98fcaa62ff
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-10 21:28:09 +0200

    bug

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,17 +5,18 @@ const bodyEl = document.querySelector("body")
 
 startButtonEl.addEventListener("click", onStartButtonClick)
 stopButtonEl.addEventListener("click", onStopButtonClick)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function changeBodyColor() {
-    return bodyEl.style.backgroundColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = getRandomHexColor();
 }
 
 function onStartButtonClick() {
     changeBodyColor();
-    let timerId = setInterval(changeBodyColor, 1000);
+    timerId = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor()}, 1000);
     startButtonEl.disabled = true;
     stopButtonEl.disabled = false;
 }
