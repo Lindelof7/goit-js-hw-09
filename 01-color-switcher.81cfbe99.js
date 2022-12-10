418b5ea263f4f5edc36c3e09391bd0b724d4d4d9
Lindelof7 <107394875+Lindelof7@users.noreply.github.com>
2022-12-10 19:01:35 +0000

    Deploying to gh-pages from @ Lindelof7/goit-js-hw-09@6db50545c8f6faf2a959336c59159179be0192d7 🚀

---
+++ 01-color-switcher.81cfbe99.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function o(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){o(),colorChanger=setInterval(o,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(colorChanger),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.81cfbe99.js.map
