98582b1c6fd0857db3e0ecf046564c7a92757f19
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-10 21:34:18 +0200

    bug fixed 100

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const startButtonEl = document.querySelector(('button[data-start]'))
 const stopButtonEl = document.querySelector(('button[data-stop]'))
 const bodyEl = document.querySelector("body")
-
+let timerId;
 
 startButtonEl.addEventListener("click", onStartButtonClick)
 stopButtonEl.addEventListener("click", onStopButtonClick)
@@ -16,7 +16,7 @@ function changeBodyColor() {
 
 function onStartButtonClick() {
     changeBodyColor();
-    timerId = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor()}, 1000);
+    timerId = setInterval(changeBodyColor, 1000);
     startButtonEl.disabled = true;
     stopButtonEl.disabled = false;
 }
